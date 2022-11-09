47b737b6cb27e6e58caf7eff5f7eab36e6f7ea61
mmaquina <mmaquina@gmail.com>
2022-11-09 19:08:25 -0300

    top3 clients rendered ok
    all report endpoints are shown ok in reports page

--- js/report/report.js
+++ js/report/report.js
@@ -1,27 +1,29 @@
+var clients
+
 fetch('http://127.0.0.1:5000/report/mri')
     .then(response => response.json())
     .then(ingredient => {
-        document.getElementById("most-requested-ingredient").innerHTML = "The most requested ingredient is " + ingredient.name + "."    
+        document.getElementById("most-requested-ingredient").innerHTML = "The most requested ingredient is <b>" + ingredient.name + "</b>."    
     });    
 
     fetch('http://127.0.0.1:5000/report/month')
     .then(response => response.json())
     .then(month => {
-        console.log(month)
-        document.getElementById("month").innerHTML = "The month with most revenue so far is " + month + "."    
+        document.getElementById("month").innerHTML = "The month with most revenue so far is <b>" + month + "</b>."    
     });    
 
     fetch('http://127.0.0.1:5000/report/top3')
     .then(response => response.json())
     .then(top3 => {
-        console.log(top3)
+        if(top3.length){
+            document.getElementById("top3").innerHTML += '<b>Top customers:</b> <ol type="1" class="list-group list-group-numbered">'
+            top3.forEach(element => {
+                document.getElementById("top3").innerHTML += "<li>"+element+"</li>"
+            })
+            document.getElementById("top3").innerHTML += "</ol>"
+        }
     });    
 
-/**
- * Find the template tag and populate it with the data
- * @param top
- */
-function createTop3Template(top) {
-    let template = $("#top-item-template")[0].innerHTML;
-    return Mustache.render(template, order);
-}
+
+
+
